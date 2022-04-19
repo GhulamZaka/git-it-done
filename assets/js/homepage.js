2fd63b96f7701f546eee395e64f879f3fc492e82
Ghulam Mustafa Zaka <g.mustafa.zaka@gmail.com>
2022-04-19 09:21:34 -0700

    creating js file

---
+++ assets/js/homepage.js
@@ -0,0 +1,11 @@
+var getUserRepos = function() {
+    fetch("https://api.github.com/users/octocat/repos");
+  };
+  
+  getUserRepos();
+
+  var response = function() {
+    console.log(response.location);
+    console.log(response.followers);
+    return
+  };
